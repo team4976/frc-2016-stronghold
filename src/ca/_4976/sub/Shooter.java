ed0e76bcb1d63cfe5f2c0f4c3d7cf523cab9eb27
Ivan-Z <izdane@gmail.com>
2016-02-16 19:40:25 -0500

    Shooter code complete

--- src/ca/_4976/sub/Shooter.java
+++ src/ca/_4976/sub/Shooter.java
@@ -1,100 +1,112 @@
 package ca._4976.sub;
 
+import ca._4976.io.Controller;
+import ca._4976.io.Input;
 import ca._4976.io.Output;
 import edu.wpi.first.wpilibj.PIDController;
 
-import static ca._4976.io.Output.*;
-import static ca._4976.io.Input.*;
-import static ca._4976.io.Controller.*;
 
+/**
+ * Created by Grant on 1/23/2016.
+ */
 public class Shooter {
 
-    PIDController pid = new PIDController(0.001, 0, 0, 0, Encoder.SHOOTER, Motor.SHOOTER);
+    PIDController pid = new PIDController(1.0e-4, 0, 0.001, 0, Input.Encoder.SHOOTER, Output.Motor.SHOOTER);
 
-    int state = 0;
-    long waitTimeFlag;
+    boolean IntakeState = true;    // false = down, true = up
+    boolean SHOOTER = false;     // true = 75%, false = golden efficiency
+    double GRIPPER = 0.0;    //true=on' false=off
+    boolean HOOD = false;
+    boolean justShot = false;
+    long delay;
 
-    public void disabledInit() {
-
-        state = 0;
-        pid.disable();
-        Motor.SHOOTER.set(0);
+    public void robotInit() {
+        pid.setSetpoint(6000);
     }
-
     public void teleopPeriodic() {
-
-        switch (state) {
-
-            default:
-                break;
-
-            case 0:
-
-
-                if (Primary.Button.B.isDownOnce()) {
-
-                    System.out.println("Shoot Pressed!");
-                    state = 1;
-                }
-
-                break;
-            case 1:
-
-                if (Digital.BALL_DETECTED.get()) {
-
-                    pid.setSetpoint(6000);
+        if (IntakeState == false) {
+            if (Controller.Primary.Button.B.isDownOnce()) {
+                Output.Solenoid.INTAKE.set(true);
+                IntakeState = true;
+                if (SHOOTER == false) {
+                    System.out.println("PID enabled");
                     pid.enable();
-                    Output.Solenoid.INTAKE.set(true);
-                    state = 2;
-
-                } else state = 0;
-
-                break;
-            case 2:
-
-                if (Primary.Button.B.isDownOnce()) {
-
-                    Motor.INTAKE_ROLLERS.set(1);
-                    state = 3;
+                    SHOOTER = true;
+                } else {
+                    Output.Motor.INTAKE_ROLLERS.set(-1);
+                    GRIPPER = -1;
                 }
+            }
+            if (Input.Digital.BALL_DETECTED.get()) {
+                Output.Motor.INTAKE_ROLLERS.set(0.0);
+            }
+            if (Controller.Secondary.Button.X.isDownOnce()) {
+                Output.Solenoid.INTAKE.set(true);
+                IntakeState = true;
+            }
+        }
+        if (IntakeState == true) {
+            if (Controller.Primary.Button.A.isDownOnce()) {
+                Output.Solenoid.INTAKE.set(false);
+                IntakeState = false;
+                pid.disable();
+                Output.Motor.SHOOTER.set(0.0);
+                SHOOTER = false;
+                Output.Motor.INTAKE_ROLLERS.set(-1);
+            }
+            if (Controller.Primary.Button.X.isDownOnce()){
+                System.out.println("Setting the shooter motor to 0");
+                pid.disable();
+                Output.Motor.SHOOTER.set(0);
+                Output.Motor.INTAKE_ROLLERS.set(0.0);
+            }
+            if (Controller.Primary.Button.B.isDownOnce()) {
 
-                break;
-            case 3:
-
-                if (!Digital.BALL_DETECTED.get()) {
-                    waitTimeFlag = System.currentTimeMillis();
-                    state = 4;
+                if (SHOOTER == true) {
+                    Output.Motor.INTAKE_ROLLERS.set(-1);
+                    GRIPPER = -1;
+                } else {
+                    System.out.println("PID enabled 2");
+                    pid.enable();
+                    SHOOTER = true;
                 }
-
-                break;
-            case 4:
-
-                if (System.currentTimeMillis() - waitTimeFlag > 500) {
-
-                    pid.disable();
-                    Motor.SHOOTER.set(0);
-                    Motor.INTAKE_ROLLERS.set(0);
-                    state = 0;
-
-                }
-                break;
+            }
 
         }
+//        if(!Input.Digital.BALL_DETECTED.get()) {
+//            if (!justShot) {
+//                delay = System.currentTimeMillis();
+//                justShot = true;
+//            } else if (System.currentTimeMillis() - delay > 500) {
+//                Output.Motor.SHOOTER.set(0);
+//                justShot = false;
+//            }
+//        }
+        if (Math.abs(Controller.Secondary.Stick.RIGHT.vertical()) > 0.1)
+            Output.Motor.INTAKE_ROLLERS.set(Controller.Secondary.Stick.RIGHT.vertical());
 
-        if (Math.abs(Secondary.Stick.LEFT.vertical()) > 0.2) {
-
-            state = -1;
-            System.out.println("Shooter RPM: " + Encoder.SHOOTER.getVelocity());
-            Motor.SHOOTER.set(Secondary.Stick.LEFT.vertical());
-
-        } else if (state == -1) {
-
-            state = 0;
-            Motor.SHOOTER.set(0);
+        if (Controller.Secondary.Button.A.isDownOnce()){
+            Output.Solenoid.INTAKE.set(!IntakeState);
+            IntakeState = !IntakeState;
+        }
+        if (Math.abs(Controller.Secondary.Stick.LEFT.vertical()) > 0.1){
+            Output.Motor.SHOOTER.set(Controller.Secondary.Stick.LEFT.vertical());
         }
 
-        if (Secondary.Button.B.isDownOnce()) Solenoid.HOOD.set(!Solenoid.HOOD.get());
+        if (Controller.Secondary.DPad.NORTH.isDownOnce()){
+            Output.Solenoid.HOOD.set(true);
+        }
+        if (Controller.Secondary.DPad.SOUTH.isDownOnce()){
+            Output.Solenoid.HOOD.set(false);
+        }
+        if (Controller.Secondary.Trigger.LEFT.value() > 0.1){
+            Output.Motor.INTAKE_ROLLERS.set(Controller.Secondary.Trigger.LEFT.value());
+        }
+        if (Controller.Secondary.Trigger.RIGHT.value() > 0.1){
+            Output.Motor.INTAKE_ROLLERS.set(Controller.Secondary.Trigger.RIGHT.value());
+        }
 
-        if (Secondary.Button.X.isDownOnce()) state = 0;
+
     }
+
 }
