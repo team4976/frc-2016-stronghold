26278e357b1882a515f76400ed4d34275a1d9da5
michaelwm <michael@miduras.com>
2016-03-10 17:11:59 -0500

    auto aim test 2

--- src/ca/_4976/sub/Targeting.java
+++ src/ca/_4976/sub/Targeting.java
@@ -16,7 +16,7 @@ public class Targeting {
         Double[] width = contours.getNumberArray("width", new Double[]{0.0});
         Double[] area = contours.getNumberArray("area", new Double[]{0.0});
 
-        double largestArea = 0.5;
+        double largestArea = 149;
         int goalI = -1;
 
         for (int i = 0; i < area.length; i++)
