151dd0b0ffca19088ad02438740a03700d9d461f
michaelwm <michael@miduras.com>
2016-03-11 11:39:58 -0500

    targetng take 3

--- src/ca/_4976/sub/Targeting.java
+++ src/ca/_4976/sub/Targeting.java
@@ -6,37 +6,55 @@ import edu.wpi.first.wpilibj.tables.ITable;
 
 public class Targeting {
 
-    public static final double ERROR = 10;
+    public static final double ERROR = 20;
 
     public ITable contours = NetworkTable.getTable("GRIP").getSubTable("GoalContours");
 
-    public void aim() {
+    public Double getEdge() {
+        Double edge = null;
 
         Double[] centerX = contours.getNumberArray("centerX", new Double[]{0.0});
         Double[] width = contours.getNumberArray("width", new Double[]{0.0});
         Double[] area = contours.getNumberArray("area", new Double[]{0.0});
 
-        double largestArea = 149.0;
+        Double largestArea = 149.0;
         int goalI = -1;
 
-        for (int i = 0; i < area.length; i++)
-            if (area[i] > largestArea) {
-                largestArea = area[i];
-                goalI = i;
-            }
-
-        if (goalI >= 0) {
-            double edge = centerX[goalI] + (width[goalI] / 2);
-            if (!(edge >= 160 - ERROR && edge <= 160 + ERROR)) {
-                if (edge < 160 - ERROR) {
-                    Output.Motor.DRIVE_LEFT.set(0.4);
-                    Output.Motor.DRIVE_RIGHT.set(0.4);
-                } else if (edge > 160 + ERROR) {
-                    Output.Motor.DRIVE_LEFT.set(-0.4);
-                    Output.Motor.DRIVE_RIGHT.set(-0.4);
+        if (area.length > 0 && centerX.length > 0 && width.length > 0)
+            for (int i = 0; i < area.length; i++)
+                if (area[i] > largestArea) {
+                    largestArea = area[i];
+                    goalI = i;
                 }
+
+        if (goalI >= 0)
+            edge = centerX[goalI] + (width[goalI] / 2);
+
+        return edge;
+    }
+
+    public boolean onTarget() {
+        Double edge = getEdge();
+        if (edge != null)
+            return (edge >= 160 - ERROR && edge <= 160 + ERROR);
+        return false;
+    }
+
+    public boolean aim() {
+        Double edge = getEdge();
+        if (!onTarget() && edge != null) {
+            if (edge < 160 - ERROR) {
+                Output.Motor.DRIVE_LEFT.set(0.4);
+                Output.Motor.DRIVE_RIGHT.set(0.4);
+            } else if (edge > 160 + ERROR) {
+                Output.Motor.DRIVE_LEFT.set(-0.4);
+                Output.Motor.DRIVE_RIGHT.set(-0.4);
             }
+        } else {
+            Output.Motor.DRIVE_LEFT.set(0.0);
+            Output.Motor.DRIVE_RIGHT.set(0.0);
         }
+        return onTarget();
     }
 
 }
